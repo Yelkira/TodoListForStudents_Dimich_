976419284b289cbc26702e642305a402140f3b4c
Yelkira <email@email.com>
2023-01-04 00:50:28 +0300

    added method map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
+import {TasksType, Todolist} from "./Todolist";
 
 function App() {
+
+    let tasks1:Array<TasksType> = [
+        {id: 1, title: "CSS", isDone:true},
+        {id: 2, title:"JS", isDone:true},
+        {id: 3, title:"React", isDone:false},
+        {id: 4, title:"Redux", isDone:false},
+    ]
+    let tasks2:Array<TasksType> = [
+        {id: 1, title:"Terminator", isDone:true},
+        {id: 2, title:"XXX", isDone:false},
+        {id: 3, title:"Gentlemen of Fortune ", isDone:true},
+    ]
+
     return (
         <div className="App">
+            <Todolist title={"What to learn"} tasks={tasks1}/>
+            <Todolist title={"Movies"} tasks={tasks2}/>
         </div>
     );
 }
