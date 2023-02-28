db15c3239c1f49e63a7eb2829a1de03d96cfd0b7
Yelkira <penda.fu@gmai.com>
2023-02-28 11:39:16 +0300

    09 - creating test, for case"ADD-TASC" addTaskAC

--- src/state/test-reducer.ts
+++ src/state/test-reducer.ts
@@ -1,33 +1,36 @@
-import {FilterValuesType, TasksStateType, TodolistType} from '../App';
-import {v1} from 'uuid';
+import {TasksStateType} from '../App';
 
 export type RemoveTaskAction1Type = {
     type: 'REMOVE-TASK',
     todolistId: string
-    taskId:string
+    taskId: string
 }
-export type ActionType12 = {
-    type: '2',
+export type addTaskACType = {
+    type: 'ADD-TASK',
     title: string
+    todolistId: string
 }
 
-type ActionsType = RemoveTaskAction1Type| ActionType12
+type ActionsType = RemoveTaskAction1Type | addTaskACType
 
-export const tasksReducer = (state: TasksStateType, action: ActionsType):TasksStateType => {
+export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
-        case "REMOVE-TASK":{
-            return {...state, [action.todolistId]:state[action.todolistId]
-                    .filter(s=>s.id!==action.taskId)}
+        case "REMOVE-TASK": {
+            return {
+                ...state, [action.todolistId]: state[action.todolistId]
+                    .filter(s => s.id !== action.taskId)
+            }
         }
-        case "2":{
-            return {...state}
+        case "ADD-TASK": {
+            const newTask = {id:"4", title:action.title, isDone: false}
+            return {...state, [action.todolistId]: [newTask,...state[action.todolistId]]}
         }
     }
 }
 
-export const removeTaskAC = (taskId: string, todolistId:string): RemoveTaskAction1Type => {
-    return { type: 'REMOVE-TASK', todolistId, taskId}
+export const removeTaskAC = (taskId: string, todolistId: string): RemoveTaskAction1Type => {
+    return {type: 'REMOVE-TASK', todolistId, taskId}
 }
-export const actionAC2 = (title: string): ActionType12 => {
-    return { type: '2', title: title}
+export const addTaskAC = (title: string, todolistId: string): addTaskACType => {
+    return {type: 'ADD-TASK', title: title, todolistId: todolistId}
 }
